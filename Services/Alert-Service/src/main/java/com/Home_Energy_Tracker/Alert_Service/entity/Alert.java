5cdfa80be5b8dfd149b4cf4347f38294d96a260e
Mohammed Khadeeruddin <36678035+khadeer0503@users.noreply.github.com>
2026-06-17 21:33:12 +0200

    Alert service (#20)
    * initiated alert service and added flyway tag v3
    
    initiated alert service and added flyway tag v3
    CREATE TABLE `alert` (
      `id` BIGINT NOT NULL AUTO_INCREMENT,
      `user_id` BIGINT,
      `sent` TINYINT(1) NOT NULL DEFAULT 0,
      `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
      PRIMARY KEY (`id`)
    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
    
    * implemented Alert service
    
    implemented Alert service

---
+++ Services/Alert-Service/src/main/java/com/Home_Energy_Tracker/Alert_Service/entity/Alert.java
@@ -0,0 +1,29 @@
+package com.Home_Energy_Tracker.Alert_Service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "alert")
+public class Alert {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private LocalDateTime createdAt;
+    private boolean sent;
+}
